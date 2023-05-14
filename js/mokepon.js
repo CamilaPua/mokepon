d16ab392d1d8fcc61b87ce22ee1e69473e29ae6a
Camila <mariacamilaarciniegaspua@gmail.com>
2023-05-13 21:03:13 -0500

    haciendo que el boton de select pet haga algo c:

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+function selectPlayerPet() {
+    alert('Seleccionaste tu mascota')
+}
+
+let buttonSelectPlayerPet = document.getElementById('button-select-pet')
+buttonSelectPlayerPet.addEventListener('click', selectPlayerPet)
