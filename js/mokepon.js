d1e1c7681e071033d63238a9112de3aa287a6db1
Camila <mariacamilaarciniegaspua@gmail.com>
2023-05-14 20:28:23 -0500

    Agrega alert que indica que mascota fue escogida, tambien muestra cuando no se selecciona una

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,24 @@
-function selectPlayerPet() {
-    alert('Seleccionaste tu mascota')
+function startGame() {
+    let buttonSelectPlayerPet = document.getElementById('button-select-pet')
+    buttonSelectPlayerPet.addEventListener('click', selectPlayerPet)
 }
 
-let buttonSelectPlayerPet = document.getElementById('button-select-pet')
-buttonSelectPlayerPet.addEventListener('click', selectPlayerPet)
+function selectPlayerPet() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let message = "You must select a pet"
+
+    if (inputHipodoge.checked) {
+        message = 'You selected Hipodoge'
+    }
+    else if (inputCapipepo.checked) {
+        message = 'You selected Capipepo'
+    }
+    else if (inputRatigueya.checked) {
+        message = 'You selected Ratigueya'
+    }
+    alert(message)
+}
+
+window.addEventListener('load', startGame)
